61f5db67b73215ab87146aa39da44acb4789e177
Mohammad Hosien Kazemi <mhkazemi23@gmail.com>
2022-05-03 15:22:37 +0430

    added regression report for GA

--- Optimizer/_report_accuracy_of_solutions.py
+++ Optimizer/_report_accuracy_of_solutions.py
@@ -1,11 +1,21 @@
+import numpy as np
 import itertools
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import mean_absolute_error as MAE
+from sklearn.metrics import mean_squared_error as MSE
+
+def R2(y_true, y_pred):
+    return np.corrcoef(y_true, y_pred)[0][1]**2
+
+def CorCoef(y_true, y_pred):
+    return np.corrcoef(y_true, y_pred)[0][1]
 
 def report_accuracy_of_solutions(df, **params):
     logger = params.get('logger')
     report_directory = params.get('report_directory')
     model_name = params.get('model_name')
     n_sections = params.get("n_sections")
+    max_n_leaks = params.get("max_n_leaks")
 
     y_true = []
     y_pred = []
@@ -31,6 +41,27 @@ def report_accuracy_of_solutions(df, **params):
     print ("------------------------------------------------")
 
 
+    y_true = []
+    y_pred = []
+    for i in range(max_n_leaks):
+
+        y_true.append(df[f'CdAl{i}-act'])
+        y_pred.append(df[f'CdAl{i}-pred'])
+
+    y_true = list(itertools.chain(*y_true))
+    y_pred = list(itertools.chain(*y_pred))
+    
+    corcoef_ = CorCoef(y_true, y_pred)
+    r2_ = R2(y_true, y_pred)
+    mse_ = MSE(y_true, y_pred)
+    mae_ = MAE(y_true, y_pred)
+
+    report_str = f"CorCoef= {corcoef_:.4f}, "\
+                    f"R2= {r2_:.4f}, RMSE={mse_**0.5:.4f}, "\
+                        f"MSE={mse_:.4f}, MAE={mae_:.4f}, "
+    
+    logger.info(report_str)
+    print(report_str)
 
 
 
--- Optimizer/_save_solutions_to_csv.py
+++ Optimizer/_save_solutions_to_csv.py
@@ -16,22 +16,28 @@ def save_solutions_to_csv(holder_of_solutions_and_info, **params):
     report_act = pd.DataFrame()
     report_pred = pd.DataFrame()
     report_xl = pd.DataFrame()
-    report_cdal = pd.DataFrame()
+    report_cdal_act = pd.DataFrame()
+    report_cdal_pred = pd.DataFrame()
 
-
+    # print(holder_of_solutions_and_info)
+    # raise ValueError
     cols_act = [f'Actual-{idx}' for idx in range(n_sections)]
     cols_pred = [f'Predicted-{idx}' for idx in range(n_sections)]
     cols_xl = [f'xL{idx}' for idx in range(max_n_leaks)]
-    cols_cdal = [f'CdAl{idx}' for idx in range(max_n_leaks)]
+    cols_cdal_act = [f'CdAl{idx}-act' for idx in range(max_n_leaks)]
+    cols_cdal_pred = [f'CdAl{idx}-pred' for idx in range(max_n_leaks)]
 
     for i in range(len(holder_of_solutions_and_info)):
 
         holder_act = holder_of_solutions_and_info[i][1].tolist()
         holder_pred = np.zeros(n_sections)
         holder_xl = holder_of_solutions_and_info[i][0][:max_n_leaks]
-        holder_cdal = holder_of_solutions_and_info[i][0][max_n_leaks:]
+        holder_cdal_act = holder_of_solutions_and_info[i][0][max_n_leaks:]
+        holder_cdal_pred = []
 
         counter = 0
+        for leak_size in holder_of_solutions_and_info[i][2]['CdAl']:
+            holder_cdal_pred.append(leak_size)
 
         for leak_loc in holder_of_solutions_and_info[i][2]['xL']:
             idx = int(np.floor(leak_loc/l_sections))
@@ -63,13 +69,17 @@ def save_solutions_to_csv(holder_of_solutions_and_info, **params):
         report_xl = pd.concat([report_xl, tmp_df], axis=0, ignore_index=True)
 
         tmp_lst = []
-        tmp_lst.append(holder_cdal)
-        tmp_df = pd.DataFrame(tmp_lst, columns=cols_cdal)    
-        report_cdal = pd.concat([report_cdal, tmp_df], axis=0, ignore_index=True)       
+        tmp_lst.append(holder_cdal_act)
+        tmp_df = pd.DataFrame(tmp_lst, columns=cols_cdal_act)    
+        report_cdal_act = pd.concat([report_cdal_act, tmp_df], axis=0, ignore_index=True)       
 
+        tmp_lst = []
+        tmp_lst.append(holder_cdal_pred)
+        tmp_df = pd.DataFrame(tmp_lst, columns=cols_cdal_pred)    
+        report_cdal_pred = pd.concat([report_cdal_pred, tmp_df], axis=0, ignore_index=True)  
 
-    report = pd.concat([report_act, report_pred, report_xl, report_cdal], axis=1)
+    report = pd.concat([report_act, report_pred, report_xl, report_cdal_act, report_cdal_pred], axis=1)
 
-    report.to_csv(report_directory + "/" + f'{model_name}-{leak_pred}.csv', index=False)
+    report.to_csv(report_directory + "/" + f'{model_name}-OnTest-{leak_pred}.csv', index=False)
 
     return report
